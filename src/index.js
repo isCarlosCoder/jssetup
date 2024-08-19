bde69ddb61113f05ab50b55819dc465d407e6177
iscarloscoder <carlosvitorcontas18@gmail.com>
2024-08-19 17:44:06 -0300

    add and testing sum function

--- src/index.js
+++ src/index.js
@@ -1,36 +1,10 @@
-// src/index.js
-
-/**
- * Função para somar dois números.
- * @param {number} a - O primeiro número.
- * @param {number} b - O segundo número.
- * @returns {number} - A soma dos dois números.
- */
-function sum(a, b) {
+const sum = (a, b) => {
   if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new Error('Os argumentos devem ser números')
+    throw new Error('Invalid arguments')
   }
   return a + b
 }
 
-/**
- * Função para subtrair dois números.
- * @param {number} a - O primeiro número.
- * @param {number} b - O segundo número.
- * @returns {number} - A diferença dos dois números.
- */
-function subtract(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new Error('Os argumentos devem ser números')
-  }
-  return a - b
+module.exports = {
+  sum,
 }
-
-// Exemplo de uso
-const resultSum = sum(10, 5)
-const resultSubtract = subtract(10, 5)
-
-console.log(`Soma: ${resultSum}`) // Soma: 15
-console.log(`Subtração: ${resultSubtract}`) // Subtração: 5
-
-module.exports = { sum, subtract }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,19 +1,23 @@
-// tests/index.test.js
+const { sum } = require('../src')
 
-const { sum, subtract } = require('../src/index')
+describe('sum', () => {
+  test('Should return the sum of two positive numbers', () => {
+    expect(sum(1, 2)).toBe(3)
+  })
 
-test('sum correctly adds two numbers', () => {
-  expect(sum(1, 2)).toBe(3)
-})
+  test('should return the sum of two negative numbers', () => {
+    expect(sum(-1, -1)).toBe(-2)
+  })
 
-test('subtract correctly subtracts two numbers', () => {
-  expect(subtract(5, 3)).toBe(2)
-})
+  test('should return the sum of a positive and a negative number', () => {
+    expect(sum(5, -3)).toBe(2)
+  })
 
-test('sum throws error when arguments are not numbers', () => {
-  expect(() => sum(1, 'a')).toThrow('Os argumentos devem ser números')
-})
+  test('should return 0 when adding 0 to 0', () => {
+    expect(sum(0, 0)).toBe(0)
+  })
 
-test('subtract throws error when arguments are not numbers', () => {
-  expect(() => subtract(5, 'b')).toThrow('Os argumentos devem ser números')
+  test('should throw an error when arguments are not numbers', () => {
+    expect(() => sum(1, 'a')).toThrow('Invalid arguments')
+  })
 })
